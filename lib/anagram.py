3eb5965518a7690d094d046e3c9d9c34e8704fbc
Shania Lopez <slopez@fandm.edu>
2023-08-26 18:30:13 -0400

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
-# your code goes here!
+
+
+class Anagram: 
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, list):
+        matches = []
+
+        for words in list:
+            if sorted(self.word) == sorted(words):
+                matches.append(words)
+            
+        return matches
